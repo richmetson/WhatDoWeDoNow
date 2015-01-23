7c77e5488c833afe5be0db800d6369a93b69bba7
Kim Barrett <kimbarrett92@gmail.com>
2015-01-23 23:56:18 +0000

    Basic patron prefab with mouth

---
+++ Assets/Scripts/PatronMouth.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AgonyBartender
+{
+    public class PatronMouth : MonoBehaviour {
+
+        public Problem PatronsProblem; 
+
+	    // Use this for initialization
+	    void Start () {
+	
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+	
+	    }
+
+        void SetSpeechBox(GUIText TextBox)
+        {
+            TextBox.text = PatronsProblem.ProblemString;
+        }
+
+    }
+}
