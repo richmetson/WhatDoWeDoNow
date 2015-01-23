b3af8c27491f5803e8ef8d12e4e957b664a221d8
Richard Fine <rfine@tbrf.net>
2015-01-23 23:29:10 +0000

    created basic ScriptableObject for 'answer' objects

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AgonyBartender
+{
+    public class Answer : ScriptableObject
+    {
+
+        public Sprite Sprite;
+        public int Weight;
+
+    }
+}
---
+++ Assets/Scripts/Editor/AnswerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace AgonyBartender.Editor
+{
+
+    [CustomEditor(typeof (Answer))]
+    public class AnswerEditor : UnityEditor.Editor
+    {
+        [MenuItem("Assets/Create/Answer")]
+        public static void CreateAsset()
+        {
+            ProjectWindowUtil.CreateAsset(CreateInstance<Answer>(), "New Answer.asset");
+        }
+    }
+
+}
