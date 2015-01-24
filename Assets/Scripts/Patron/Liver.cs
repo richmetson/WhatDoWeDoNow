55a2eb457cef203f390fb3ea4c608021a716d832
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 10:37:46 +0000

    Created liver component

---
+++ Assets/Scripts/Patron/Liver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AgonyBartender
+{
+    interface IAlchoholAffectedSystem
+    {
+        void OnDrunkennessAdjusted(float ABV);
+    }
+
+    [RequireComponent(typeof(PatronDefinition))]
+    public class Liver : MonoBehaviour {
+
+        public float StartingABV;
+
+        float CurrentABV;
+
+	    // Use this for initialization
+	    void Start () {
+            CurrentABV = StartingABV;
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+	
+	    }
+
+        public void AdjustDrunkeness(float DeltaDrunkeness)
+        {
+            CurrentABV += DeltaDrunkeness;
+
+            gameObject.SendMessage("OnDrunkennessAdjusted", CurrentABV, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
