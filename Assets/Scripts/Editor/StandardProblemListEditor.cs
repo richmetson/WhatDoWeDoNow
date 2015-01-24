f229847e14a5ed6435c5d740cf548f8b89245d1e
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 14:23:11 +0000

    Created a global problem list

---
+++ Assets/Scripts/Editor/StandardProblemListEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace AgonyBartender.Editor
+{
+    [CustomEditor(typeof(StandardProblemList))]
+    public class StandardProblemListEditor : UnityEditor.Editor
+    {
+
+        [MenuItem("Assets/Create/StandardProblemList")]
+        public static void CreateAsset()
+        {
+            ProjectWindowUtil.CreateAsset(CreateInstance<StandardProblemList>(), "New StandardProblemList.asset");
+        }
+
+        private SerializedProperty _problemsArray;
+
+        private void OnEnable()
+        {
+            _problemsArray = serializedObject.FindProperty("GlobalProblems");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_problemsArray, true);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
