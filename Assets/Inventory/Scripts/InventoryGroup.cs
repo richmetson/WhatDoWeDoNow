9c61d4954e8d06dce8240145a1f860d3d3cd0320
Richard Fine <rfine@tbrf.net>
2015-01-24 23:18:17 +0000

    hide/show the inventory when dragging stuff / on mouseover

---
+++ Assets/Inventory/Scripts/InventoryGroup.cs
@@ -0,0 +1,38 @@
+﻿using AgonyBartender.Inventory;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class InventoryGroup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Animator Animator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        IsPointerInside = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsPointerInside = false;
+    }
+
+    public bool IsPointerInside { get; private set; }
+
+    private bool _isShowing;
+
+    public void Update()
+    {
+        var currentAnimation = Animator.GetCurrentAnimatorStateInfo(0);
+
+        bool shouldShow = IsPointerInside || Inventory.Default.IsDraggingItem;
+        if (shouldShow != _isShowing)
+        {
+            float offset = 1f - Mathf.Clamp01(currentAnimation.normalizedTime);
+
+            Animator.Play(shouldShow ? "ShowInventory" : "HideInventory", 0, offset);
+            _isShowing = shouldShow;
+        }
+    }
+}
