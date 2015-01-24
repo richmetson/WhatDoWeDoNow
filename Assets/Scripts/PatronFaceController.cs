fce00991c049653d6a43b6f33f7a3ee3d75b0a9c
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 02:45:03 +0000

    Implemented the face controller

---
+++ Assets/Scripts/PatronFaceController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+namespace AgonyBartender
+{
+    [RequireComponent(typeof(PatronDefinition))]
+    [RequireComponent(typeof(Image))]
+    public class PatronFaceController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            OnFaceChanged(ProblemSolutionFacialExpression.NeutralResponse);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnFaceChanged(ProblemSolutionFacialExpression NewExpression)
+        {
+            if(NewExpression == ProblemSolutionFacialExpression.FacialExpressionCount)
+            {
+                Debug.LogError("Invalid facial expression");
+            }
+
+            PatronDefinition Patron = gameObject.GetComponent<PatronDefinition>();
+            Sprite NewFace = Patron.Patron.FaceSprites[(int)NewExpression];
+
+            Image ImageComponent = gameObject.GetComponent<Image>();
+            ImageComponent.sprite = NewFace;
+        }
+    }
+
+}
