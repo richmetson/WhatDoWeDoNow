b7ee2f4b8da8b9a50cdec5cc7e1eacdfa4a141f0
Rich Metson <richardmetson@gmail.com>
2015-01-24 00:01:48 +0000

    Patron Scriptable Object check in

---
+++ Assets/Scripts/Patron.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AgonyBartender
+{
+	public class Patron : ScriptableObject
+	{
+		public string PatronName;
+		public Sprite Sprite;
+		public Problem[] PatronsProblems;  
+
+	}
+}
