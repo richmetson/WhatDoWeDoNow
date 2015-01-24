54485b62a8a17bc7c0e4ce89a587d4edf2831da8
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 10:56:06 +0000

    FaceController gets drunk and wobbles

--- Assets/Scripts/Patron/PatronFaceController.cs
+++ Assets/Scripts/Patron/PatronFaceController.cs
@@ -7,19 +7,50 @@ namespace AgonyBartender
 {
     [RequireComponent(typeof(PatronDefinition))]
     [RequireComponent(typeof(Image))]
-    public class PatronFaceController : MonoBehaviour
+    public class PatronFaceController : MonoBehaviour, IAlchoholAffectedSystem
     {
+
+        float FacialRotation;
+        public float WobblePeriod;
+
+        public float MaxRotation = 25.0f;
 
         // Use this for initialization
         void Start()
         {
             OnFaceChanged(ProblemSolutionFacialExpression.NeutralResponse);
+            StartCoroutine(WobbleHead());
         }
 
         // Update is called once per frame
         void Update()
         {
+        }
 
+        IEnumerator WobbleHead()
+        {
+            Image ImageComponent = gameObject.GetComponent<Image>();
+            while (true)
+            {
+                print(FacialRotation);
+                for (float Rotation = 0.0f; Rotation <= FacialRotation; Rotation += (FacialRotation / WobblePeriod) * Time.deltaTime)
+                {
+                    ImageComponent.transform.rotation = Quaternion.Euler(0, 0, Rotation);
+                    yield return null;
+                }
+
+                for (float Rotation = FacialRotation; Rotation >= -FacialRotation; Rotation -= (FacialRotation / WobblePeriod) * Time.deltaTime)
+                {
+                    ImageComponent.transform.rotation = Quaternion.Euler(0, 0, Rotation);
+                    yield return null;
+                }
+
+                for (float Rotation = -FacialRotation; Rotation <= 0.0f; Rotation += (FacialRotation / WobblePeriod) * Time.deltaTime)
+                {
+                    ImageComponent.transform.rotation = Quaternion.Euler(0, 0, Rotation);
+                    yield return null;
+                }
+            }
         }
 
         public void OnFaceChanged(ProblemSolutionFacialExpression NewExpression)
@@ -29,6 +60,11 @@ namespace AgonyBartender
 
             Image ImageComponent = gameObject.GetComponent<Image>();
             ImageComponent.sprite = NewFace;
+        }
+
+        public void OnDrunkennessAdjusted(float ABV)
+        {
+            FacialRotation = ABV * MaxRotation;
         }
     }
 
