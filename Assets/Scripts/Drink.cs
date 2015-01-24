e13c9bb1b7d9d79919a0cb0209c2e795d6b56b2b
Richard Fine <rfine@tbrf.net>
2015-01-24 10:58:25 +0000

    a thicker head on the pint, and click+hold to top up

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 [ExecuteInEditMode]
-public class Drink : MonoBehaviour
+public class Drink : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
 
     public Image BeerImage;
@@ -11,12 +13,14 @@ public class Drink : MonoBehaviour
     public int FullHeadOffset = 74;
     public int EmptyHeadOffset = -120;
 
+    public float TopUpRate = 0.1f;
+
     [SerializeField, Range(0, 1)] private float _level;
 
     public float Level
     {
         get { return _level; }
-        set { _level = value;
+        set { _level = Mathf.Clamp01(value);
             SyncSprites();
         }
     }
@@ -30,5 +34,32 @@ public class Drink : MonoBehaviour
     {
         BeerImage.fillAmount = _level;
         BeerHead.localPosition = new Vector3(0, Mathf.Lerp(EmptyHeadOffset, FullHeadOffset, _level), 0);
+    }
+
+    private bool _isFilling;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isFilling = true;
+        StartCoroutine(FillGlass());
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isFilling = false;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isFilling = false;
+    }
+
+    private IEnumerator FillGlass()
+    {
+        while (_isFilling)
+        {
+            Level += Time.deltaTime*TopUpRate;
+            yield return null;
+        }
     }
 }
