8e194f5c9fa12758f43c0d48c64e04e7ff693a89
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 11:11:24 +0000

    Added the beer hand to the patron

---
+++ Assets/Scripts/Patron/BeerHand.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace AgonyBartender
+{
+    [RequireComponent(typeof(PatronDefinition))]
+    public class BeerHand : MonoBehaviour
+    {
+        public Drink Beer;
+
+        public RangedFloat GapBetweenGulps;
+        public RangedFloat LengthOfGulp;
+
+        bool IsDrinking;
+
+        // Use this for initialization
+        IEnumerator Start()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(GapBetweenGulps.PickRandom());
+                IsDrinking = true;
+                yield return new WaitForSeconds(LengthOfGulp.PickRandom());
+                IsDrinking = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(IsDrinking)
+            {
+                if(!Beer)
+                {
+                    Debug.LogError("WHERE IS MY BEER!!!");
+                    return;
+                }
+
+                Beer.Level = Beer.Level - 0.1f * Time.deltaTime;
+            }
+        }
+    }
+}
