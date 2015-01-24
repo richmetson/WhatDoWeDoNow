bc5bd05b572ab0af3ff7697952f737529f0e3472
Kim Barrett <kimbarrett92@gmail.com>
2015-01-24 01:49:10 +0000

    Problem solutions now have facial expressions

--- Assets/Scripts/Problem.cs
+++ Assets/Scripts/Problem.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 
 namespace AgonyBartender
 {
+    public enum ProblemSolutionFacialExpression
+    {
+        HappyResponse,
+        NeutralResponse, 
+        AngryResponse,
+        ConfusedResponse
+    }
+
     [System.Serializable]
     public struct ProblemSolution
     {
         public Answer Answer;
         public int Score;
+        public ProblemSolutionFacialExpression FacialOutcome;
     }
 
     public class Problem : ScriptableObject
