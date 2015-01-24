731aa6ae0f7d76656ff39b5e91d0ccfb506e6a99
Richard Fine <rfine@tbrf.net>
2015-01-24 17:45:57 +0000

    main menu scene

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AgonyBartender.Frontend
+{
+
+    public class MainMenuController : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            
+        }
+
+        public void Credits()
+        {
+            
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+    }
+
+}
