d9219a8e884b1f0436a42b4b4e93c859a2a08fc0
Richard Fine <rfine@tbrf.net>
2015-01-24 09:45:37 +0000

    custom editor for Patron to make it easier to see which sprite is which

--- Assets/Scripts/Editor/PatronEditor.cs
+++ Assets/Scripts/Editor/PatronEditor.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 
 namespace AgonyBartender.Editor
 {
@@ -10,5 +12,34 @@ namespace AgonyBartender.Editor
 		{
 			ProjectWindowUtil.CreateAsset(CreateInstance<Patron>(), "New Patron.asset");
 		}
+
+	    private SerializedProperty _patronName;
+	    private SerializedProperty _faceSpritesArray;
+	    private SerializedProperty _problemsArray;
+
+	    private ProblemSolutionFacialExpression[] _faceExpressions;
+
+	    private void OnEnable()
+	    {
+	        _patronName = serializedObject.FindProperty("PatronName");
+	        _faceSpritesArray = serializedObject.FindProperty("FaceSprites");
+	        _problemsArray = serializedObject.FindProperty("PatronsProblems");
+
+	        _faceExpressions = (ProblemSolutionFacialExpression[])Enum.GetValues(typeof (ProblemSolutionFacialExpression));
+	        _faceSpritesArray.arraySize = _faceExpressions.Length;
+	    }
+
+	    public override void OnInspectorGUI()
+	    {
+	        EditorGUILayout.PropertyField(_patronName);
+
+	        for (int i = 0; i < _faceSpritesArray.arraySize; ++i)
+	        {
+	            var expr = _faceSpritesArray.GetArrayElementAtIndex(i);
+	            EditorGUILayout.PropertyField(expr, new GUIContent(_faceExpressions[i].ToString()));
+	        }
+
+	        EditorGUILayout.PropertyField(_problemsArray, true);
+	    }
 	}
 }
