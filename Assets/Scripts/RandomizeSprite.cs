76fec8c27d6e20ab8de95d60e5c1bc62b4ea822b
Richard Fine <rfine@tbrf.net>
2015-01-25 17:22:09 +0000

    bystander graphics

--- Assets/Scripts/RandomizeSprite.cs
+++ Assets/Scripts/RandomizeSprite.cs
@@ -7,10 +7,18 @@ namespace AgonyBartender
         public UnityEngine.UI.Image Target;
 
         public Sprite[] Sprites;
+        public bool SetNativeSize;
+        public float ScaleFactor = 1f;
 
         public void OnEnable()
         {
             Target.sprite = Sprites.Random();
+
+            if (SetNativeSize)
+            {
+                Target.GetComponent<RectTransform>().sizeDelta = new Vector2(Target.sprite.rect.width,
+                    Target.sprite.rect.height) * ScaleFactor;
+            }
         }
     }
 
